7fb476a41db7d4cc12846e3e0ee28bc66c970cca
walarconc <w.alarconc@uniandes.edu.co>
2025-02-25 18:36:08 -0500

    Update handlers.py

--- src/aeroalpes/modulos/vuelos/aplicacion/handlers.py
+++ src/aeroalpes/modulos/vuelos/aplicacion/handlers.py
@@ -6,21 +6,25 @@ class HandlerReservaIntegracion(Handler):
 
     @staticmethod
     def handle_reserva_creada(evento):
+        print("Entra handle_reserva_creada " + evento)
         despachador = Despachador()
         despachador.publicar_evento(evento, 'eventos-reserva')
 
     @staticmethod
     def handle_reserva_cancelada(evento):
+        print("Entra handle_reserva_cancelada " + evento)
         despachador = Despachador()
         despachador.publicar_evento(evento, 'eventos-reserva')
 
     @staticmethod
     def handle_reserva_aprobada(evento):
+        print("Entra handle_reserva_aprobada " + evento)
         despachador = Despachador()
         despachador.publicar_evento(evento, 'eventos-reserva')
 
     @staticmethod
     def handle_reserva_pagada(evento):
+        print("Entra handle_reserva_pagada " + evento)
         despachador = Despachador()
         despachador.publicar_evento(evento, 'eventos-reserva')
 
