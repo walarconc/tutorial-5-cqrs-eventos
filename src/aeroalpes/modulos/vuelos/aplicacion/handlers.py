960ea564ec395a0a24cac6cb87c01a17e7392e85
walarconc <w.alarconc@uniandes.edu.co>
2025-02-25 19:35:26 -0500

    Update handlers.py

--- src/aeroalpes/modulos/vuelos/aplicacion/handlers.py
+++ src/aeroalpes/modulos/vuelos/aplicacion/handlers.py
@@ -6,27 +6,35 @@ class HandlerReservaIntegracion(Handler):
 
     @staticmethod
     def handle_reserva_creada(evento):
-        print("Entra handle_reserva_creada " + evento)
+        print("=============Entra handle_reserva_creada===========")
+        print(evento)        
         despachador = Despachador()
         despachador.publicar_evento(evento, 'eventos-reserva')
+        print("=============FIN handle_reserva_creada===========")
 
     @staticmethod
     def handle_reserva_cancelada(evento):
-        print("Entra handle_reserva_cancelada " + evento)
+        print("=============Entra handle_reserva_cancelada===========")
+        print(evento)        
         despachador = Despachador()
         despachador.publicar_evento(evento, 'eventos-reserva')
+        print("=============FIN handle_reserva_cancelada===========")
 
     @staticmethod
     def handle_reserva_aprobada(evento):
-        print("Entra handle_reserva_aprobada " + evento)
+        print("=============Entra handle_reserva_aprobada===========")
+        print(evento)        
         despachador = Despachador()
         despachador.publicar_evento(evento, 'eventos-reserva')
+        print("=============FIN handle_reserva_aprobada===========")
 
     @staticmethod
     def handle_reserva_pagada(evento):
-        print("Entra handle_reserva_pagada " + evento)
+        print("=============Entra handle_reserva_pagada===========")
+        print(evento)        
         despachador = Despachador()
         despachador.publicar_evento(evento, 'eventos-reserva')
+        print("=============FIN handle_reserva_pagada===========")
 
 
     
